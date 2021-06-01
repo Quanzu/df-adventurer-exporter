30feea0699d1adc48cea76a2e2280c72d0455fb5
Dustin Nguyen <Dhn74201@uga.edu>
2021-06-01 00:03:59 -0400

    Added appearance export

--- exportadv.lua
+++ exportadv.lua
@@ -1,25 +1,18 @@
 -- Export adventurer data into an JSON file for use in importadv script.
--- Script version: v0.1.0
+-- Script version: v0.2.0
 -- For DF v0.47.05
 --[====[
-
 exportadv
 =============
 Export adventurer data into an JSON file for use in importadv script.
-
 Currently, this script only exports adventurer skills, attributes, and professions.
 Future iterations will hopefully be able to export more data to more accurately
 reconstruct an adventurer.
-
 This script will create a file in the format:
     exported_adventurers/{first_name}-{save_folder}-YYYYY-MM-DD-adventurer.json
-
 Simply enter adventure mode with your character and run this script!
-
 Usage::
-
     exportadv
-
 ]====]
 
 local json = require "json"
@@ -79,20 +72,19 @@ end
 -- Unit parsing
 unit_export_data = {
     name = "default",
+    race = -1,
+    sex = -1,
     profession = -1,
     profession2 = -1,
+    appearance = {},
+    body_metadata = {},
+    personality = {},
     skills = {},
     body_attributes = {},
     mental_attributes = {}
 }
 
-function get_unit_attributes(target)
-    if target == nil then
-        print("No unit available!")
-        return
-    end
-    local unit = target
-
+function get_unit_attributes(unit)
     for attribute_name, attribute in pairs(unit.body.physical_attrs) do
         attr_data = Attribute:new(attribute_name, attribute.value, attribute.max_value)
         table.insert(unit_export_data.body_attributes, attr_data)
@@ -104,28 +96,92 @@ function get_unit_attributes(target)
     end
 end
 
-function get_unit_skills(target)
-    if target == nil then
-        print("No unit available!")
-        return
-    end
-    local unit = target
-
+function get_unit_skills(unit)
     for index, skill in ipairs(unit.status.current_soul.skills) do
         local skill_data = Skill:new(skill.id, skill.rating, skill.experience)
         table.insert(unit_export_data.skills, skill_data)
     end
 end
 
-function get_unit_metadata(target)
-    if target == nil then
-        print("No unit available!")
-        return
-    end
-    local unit = target
+function get_unit_body_metadata(unit)
+    unit_export_data.body_metadata.weapon_bp = unit.body.weapon_bp
+    unit_export_data.body_metadata.blood_max = unit.body.blood_max
+    unit_export_data.body_metadata.size_info = {
+        size_cur = unit.body.size_info.size_cur,
+        size_base = unit.body.size_info.size_base,
+        area_cur = unit.body.size_info.area_cur,
+        area_base = unit.body.size_info.area_base,
+        length_cur = unit.body.size_info.length_cur,
+        length_base = unit.body.size_info.length_base
+    }
+end
 
-    -- Note that the name is only used for the exported file name.
+function get_unit_appearance(unit)
+    unit_export_data.appearance.body_modifiers = {}
+    for id, value in pairs(unit.appearance.body_modifiers) do
+        modifier = {id = id, value = value}
+        table.insert(unit_export_data.appearance.body_modifiers, modifier)
+    end
+
+    unit_export_data.appearance.bp_modifiers = {}
+    for id, value in pairs(unit.appearance.bp_modifiers) do
+        modifier = {id = id, value = value}
+        table.insert(unit_export_data.appearance.bp_modifiers, modifier)
+    end
+
+    unit_export_data.appearance.size_modifier = unit.appearance.size_modifier
+
+    unit_export_data.appearance.tissue_style = {}
+    for id, value in pairs(unit.appearance.tissue_style) do
+        ts = {id = id, value = value}
+        table.insert(unit_export_data.appearance.tissue_style, ts)
+    end
+
+    unit_export_data.appearance.tissue_style_civ_id = {}
+    for id, value in pairs(unit.appearance.tissue_style_civ_id) do
+        ts = {id = id, value = value}
+        table.insert(unit_export_data.appearance.tissue_style_civ_id, ts)
+    end
+
+    unit_export_data.appearance.tissue_style_id = {}
+    for id, value in pairs(unit.appearance.tissue_style_id) do
+        ts = {id = id, value = value}
+        table.insert(unit_export_data.appearance.tissue_style_id, ts)
+    end
+
+    unit_export_data.appearance.tissue_style_type = {}
+    for id, value in pairs(unit.appearance.tissue_style_type) do
+        ts = {id = id, value = value}
+        table.insert(unit_export_data.appearance.tissue_style_type, ts)
+    end
+
+    unit_export_data.appearance.tissue_length = {}
+    for id, value in pairs(unit.appearance.tissue_length) do
+        ts = {id = id, value = value}
+        table.insert(unit_export_data.appearance.tissue_length, ts)
+    end
+
+    unit_export_data.appearance.colors = {}
+    for id, value in pairs(unit.appearance.colors) do
+        color = {id = id, value = value}
+        table.insert(unit_export_data.appearance.colors, color)
+    end
+
+    unit_export_data.appearance.genes = {appearance = {}, colors = {}}
+    for id, value in pairs(unit.appearance.genes.appearance) do
+        gene = {id = id, value = value}
+        table.insert(unit_export_data.appearance.genes.appearance, gene)
+    end
+    for id, value in pairs(unit.appearance.genes.colors) do
+        gene = {id = id, value = value}
+        table.insert(unit_export_data.appearance.genes.colors, gene)
+    end
+end
+
+function get_unit_metadata(unit)
     unit_export_data.name = unit.name.first_name
+    unit_export_data.race = unit.race
+    unit_export_data.sex = unit.sex
     unit_export_data.profession = unit.profession
     unit_export_data.profession2 = unit.profession2
 end
@@ -136,6 +192,8 @@ function data_preview(export_data)
     print("\nExporting adventurer with the following data...\n")
 
     print("name: " .. export_data.name)
+    print("race: " .. export_data.race)
+    print("sex: " .. export_data.sex)
     print("profession: " .. export_data.profession)
     print("profession2: " .. export_data.profession2)
 
@@ -191,8 +249,14 @@ end
 
 function export_adv()
     local target = df.global.world.units.active[0]
+    if not target then
+        qerror("No valid unit selected!")
+    end
+
     get_unit_attributes(target)
     get_unit_skills(target)
+    get_unit_appearance(target)
+    get_unit_body_metadata(target)
     get_unit_metadata(target)
     data_preview(unit_export_data)
     export_to_json(unit_export_data)
